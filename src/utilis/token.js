cfefdee8e5960d8b002d8976ff438259b1275a62
gkl <gkl@qq.com>
2020-02-24 10:07:37 +0800

    登录模块--登录功能和token抽取

---
+++ src/utilis/token.js
@@ -0,0 +1,17 @@
+//定义一个常量
+const TOKENKEY = 'mmtoken'
+//获取token
+export function getToken(){
+    return window.localStorage.getItem(TOKENKEY)
+}
+
+//设置token
+export function setToken(token){
+     window.localStorage.setItem(TOKENKEY,token)
+
+}
+
+//删除token
+export function removeToken(){
+    window.localStorage.removeItem(TOKENKEY)
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -16,7 +16,12 @@
         </el-form-item>
 
         <el-form-item prop="password">
-          <el-input v-model="form.password" prefix-icon="el-icon-lock" show-password placeholder="请输入密码"></el-input>
+          <el-input
+            v-model="form.password"
+            prefix-icon="el-icon-lock"
+            show-password
+            placeholder="请输入密码"
+          ></el-input>
         </el-form-item>
 
         <el-form-item prop="code">
@@ -25,24 +30,24 @@
               <el-input v-model="form.code" prefix-icon="el-icon-key" placeholder="请输入验证码"></el-input>
             </el-col>
             <el-col :span="7">
-              <img class="code" src="./images/login_captcha.png" alt />
+              <img class="code" :src="imgUrl" @click="getImgUrl" alt />
             </el-col>
           </el-row>
         </el-form-item>
 
-        <el-form-item class="checks" prop="agree" >
-           <div class="check">
-          <el-checkbox v-model="form.agree">
-               我已阅读并同意
+        <el-form-item class="checks" prop="agree">
+          <div class="check">
+            <el-checkbox v-model="form.agree">
+              我已阅读并同意
               <el-link type="primary">用户协议</el-link>和
-            <el-link type="primary">隐私条款</el-link>
+              <el-link type="primary">隐私条款</el-link>
             </el-checkbox>
-            </div>
+          </div>
         </el-form-item>
 
         <el-form-item>
-          <el-button @click="goLogin" class="el-button1" type='primary'>登录</el-button>
-          <el-button class="el-button1" type='primary' @click="goRegist">注册</el-button>
+          <el-button @click="goLogin" class="el-button1" type="primary">登录</el-button>
+          <el-button class="el-button1" type="primary" @click="goRegist">注册</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -55,51 +60,93 @@
 </template>
 
 <script>
-import reg from './components/regist'
+import reg from "./components/regist";
+import { login } from "@/api/login.js";
+import {getToken} from '@/utilis/token.js'
 export default {
-  components:{
+  components: {
     reg
   },
-  created(){
+  created() {
     console.log(process.env.VUE_APP_URL);
-    
   },
   data() {
     return {
+      imgUrl: process.env.VUE_APP_URL + "/captcha?type=login",
       //跟表单双向绑定的数据
-      form:{
-        phone:'',
-        password:'',
-        code:'',
-        agree:false
+      form: {
+        phone: "",
+        password: "",
+        code: "",
+        agree: false
       },
       // 规则对象
-      rules:{
-        phone:[{
-          required:true, message: "手机号不能为空", trigger: "blur"
-        }],
-        password:[{
-          required:true, message: "密码", trigger: "blur"
-        }],
-        code:[{
-          required:true, message: "验证码", trigger: "blur"
-        }],
-        agree:[{
-          pattern:/true/, message: "必须勾选同意用户协议", trigger: "change"
-        }],
+      rules: {
+        phone: [
+          {
+            required: true,
+            message: "手机号不能为空",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "密码",
+            trigger: "blur"
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: "验证码",
+            trigger: "blur"
+          }
+        ],
+        agree: [
+          {
+            pattern: /true/,
+            message: "必须勾选同意用户协议",
+            trigger: "change"
+          }
+        ]
       }
-    }
+    };
   },
-  methods:{
-    goLogin(){
-      this.$refs.loginForm.validate(v=>{
-        if(v){
-          alert('全部通过')
-        }
-      })
+  methods: {
+    //验证码刷新
+    getImgUrl() {
+      this.imgUrl =
+        process.env.VUE_APP_URL + "/captcha?type=login&t=" + Date.now();
     },
-    goRegist(){
-      this.$refs.reg.dialogFormVisible=true
+    //登录点击事件
+    goLogin() {
+      this.$refs.loginForm.validate(v => {
+        if (v) {
+          // alert('全部通过')
+          //用户登录
+          login({
+            phone:this.form.phone,
+            password:this.form.password,
+            code: this.form.code
+          }).then(res=>{
+            window.console.log(res);
+            if(res.data.code==200){
+              // /吧token存起来
+              // window.localStorage.setItem('token',res.data.data.token)
+              getToken(res.data.data.token)
+              this.$message.success('登录成功')
+              this.$router.push('/index')
+            }else{
+              this.$message.error(res.data.message)
+            }
+            
+          })
+        }
+      });
+    },
+    goRegist() {
+      this.$refs.reg.dialogFormVisible = true;
     }
   }
 };
@@ -123,8 +170,6 @@ export default {
     height: 550px;
     background: rgba(245, 245, 245, 1);
     padding-right: 42px;
-
-    
 
     .title-box {
       display: flex;
@@ -155,25 +200,24 @@ export default {
     }
   }
 
-  .code{
+  .code {
     width: 100%;
     height: 42px;
     vertical-align: top;
   }
-  .checks{
+  .checks {
     display: flex;
-    .check{
-    display: flex;
-    align-items: center
-  }
-  }
-  .el-button1{
-    width: 100%;
-    &:nth-child(2){
-      margin-left: 0;
-      margin-top: 26px
+    .check {
+      display: flex;
+      align-items: center;
     }
   }
-  
+  .el-button1 {
+    width: 100%;
+    &:nth-child(2) {
+      margin-left: 0;
+      margin-top: 26px;
+    }
+  }
 }
 </style>
